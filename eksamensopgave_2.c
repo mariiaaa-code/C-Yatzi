7375a76f1e2565ea7248699bd74fc7a04196743c
Maria <mariadehn10@gmail.com>
2024-11-17 20:32:54 +0100

    Intial commit

---
+++ eksamensopgave_2.c
@@ -0,0 +1,39 @@
+//Maria Lilly Nielsen | mlni24@student.aau.dk | Cyber- og computerteknologi
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int roll_multiple_dice
+
+//indtast n terninger
+int main() {
+    int terninger;
+    printf("Indtast hvor mange terninger du vil bruge ");
+    scanf("%d", &terninger);
+
+    return 0;
+}
+
+int roll_multiple_dice {
+    
+}
+
+//tabel over resultat (int array)
+//rand funktion roll_multiple_dice, der laver int array og viser n samtidige terningekast
+
+//1'ere
+//2'ere
+//3'ere
+//4'ere
+//5'ere
+//6'ere
+//par 1
+//par 2 (skal være to forskellige slags)
+//3 ens
+//4 ens
+//small straight: 1-2-3-4-5 (15 points)
+//large straight: 2-3-4-5-6 (20 points)
+//full house
+//chance
+//yatzi
+
